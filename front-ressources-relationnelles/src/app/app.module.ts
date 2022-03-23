f2bac727b619cc3b73abe0ee4a97c3ad1e829d85
Hugo Mercier <mercier.hugo14@gmail.com>
2022-03-23 19:42:54 +0100

    feat: create the component for the left side bar

--- front-ressources-relationnelles/src/app/app.module.ts
+++ front-ressources-relationnelles/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LeftSideBarComponent } from './left-side-bar/left-side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftSideBarComponent
   ],
   imports: [
     BrowserModule
