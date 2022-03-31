737811eccf97dea377e7a3e494215d8ea4a491fc
AugustinDmrt <55685689+AugustinDmrt@users.noreply.github.com>
2022-03-31 16:37:55 +0200

    Création menu burger
    Création d'un menu burger de la nav bar quand l'écran est trop petit

--- front-ressources-relationnelles/src/app/left-side-bar/left-side-bar.component.ts
+++ front-ressources-relationnelles/src/app/left-side-bar/left-side-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-left-side-bar',
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeftSideBarComponent implements OnInit {
 
-  constructor() { }
+  private x:boolean = false;
+  // private menuContainer:any = document.getElementById("menu-container");
+
+  constructor() {
+
+  }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+
+
+
+  }
+  openMenu() {
+    let menuContainer = document.getElementById("menu-container");
+    let menuBurger = document.getElementById("menu-burger");
+
+    // @ts-ignore
+    menuContainer.style.display="flex";
+    // @ts-ignore
+    menuBurger.style.display="none";
+
+  }
+
+
+  closeMenu() {
+    let menuContainer = document.getElementById("menu-container");
+    let menuBurger = document.getElementById("menu-burger");
+
+    // @ts-ignore
+    menuContainer.style.display="none";
+    // @ts-ignore
+    menuBurger.style.display="flex";
+  }
 }
