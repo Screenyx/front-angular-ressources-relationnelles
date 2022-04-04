4d81f70c0422bdc1e2c1754f39a14e28d076d9c3
Screenyx <mercier.hugo14@gmail.com>
2022-04-04 16:18:06 +0200

    add service resources to comunicate with the api

--- src/app/ressources/ressources.component.ts
+++ src/app/ressources/ressources.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { RessourcesService } from './ressources.service';
 
 @Component({
   selector: 'app-ressources',
   templateUrl: './ressources.component.html',
-  styleUrls: ['./ressources.component.css']
+  styleUrls: ['./ressources.component.css'],
+  providers: [RessourcesService]
 })
 export class RessourcesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ressourceService: RessourcesService) { }
 
   ngOnInit(): void {
+    this.ressourceService.getRessources().subscribe(value => {
+      console.log(value);
+    })
   }
 
 }
